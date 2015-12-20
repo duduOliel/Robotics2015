26d91edc2ce1535b91bec5fbd0fc594e7a68aa27
Dudu <oliel@yahoo.com>
2015-12-19 16:41:59 -0800

    Some performance impruvments

--- DuduRobot/Map.cpp
+++ DuduRobot/Map.cpp
@@ -23,15 +23,15 @@ void Map::loadImage(){
 	lodepng::decode(image, mapWidth, mapHeight, mapFile);
 	cout<<"Image size: " << image.size() << " Map width: " << mapWidth << " Map height: " << mapHeight << endl;
 
-	for (unsigned int i = 0 ; i < mapHeight ; ++i){
-		vector<bool> row;
-		for (unsigned int j = 0 ; j < mapWidth; ++j){
-			row.push_back(image[((i * mapWidth) + j) * 4] == 0);
+	map.resize(mapHeight);
+	for (unsigned int i = 0 ; i < mapHeight ; i++){
+		map[i].resize(mapWidth);
+		for (unsigned int  j = 0 ; j < mapWidth ; j++){
+			map[i][j] = image[((i * mapWidth) + j) * 4] == 0;
 		}
-
-		map.push_back(row);
 	}
 
+	// Pring map
 	for (unsigned int i = 0 ; i < mapHeight; ++i){
 		for (unsigned int j = 0 ; j < mapWidth; ++j){
 			cout << (map[i][j] ? "0" : " ");
@@ -43,10 +43,10 @@ void Map::loadImage(){
 void Map::inflateGrig(unsigned int robotSizeInCells){
 	cout << "Inflating map" <<endl;
 	unsigned int inflateRadius = robotSizeInCells / 2;
-	Grid inflated;
 
 	unsigned int mapWidth = map[0].size();
 	unsigned int mapHeight = map.size();
+
 
 	for (unsigned int i = 0 ; i < mapHeight ; i++){
 		vector<bool> row;
@@ -65,24 +65,35 @@ void Map::inflateGrig(unsigned int robotSizeInCells){
 		inflated.push_back(row);
 	}
 
-	for (unsigned int i = 0 ; i < mapHeight; ++i){
-		for (unsigned int j = 0 ; j < mapWidth; ++j){
+	// print inflated map
+	for (unsigned int i = 0 ; i < mapHeight; i++){
+		for (unsigned int j = 0 ; j < mapWidth; j++){
 			cout << (inflated[i][j] ? "0" : " ");
 		}
 		cout<<"\n"<<endl;
 	}
-
-	// Clearing old map
-	for (unsigned int i = 0 ; i < mapHeight ; i++){
-		map[i].clear();
-	}
-	map.clear();
-
-	map = inflated;
 }
 
-void Map::createFineGrid(){
-//	unsigned int fineGridWidth = map
+void Map::createFineGrid(unsigned int robotSizeInCells){
+	unsigned int fineWidth = inflated[0].size() / robotSizeInCells;
+	unsigned int fineHeight = inflated.size() / robotSizeInCells;
+
+	for (unsigned int i = 0 ; i < fineHeight ; i++){
+			vector<bool> row;
+
+			for (unsigned int j = 0 ; j < fineWidth ; j++){
+				bool cellValue = false;
+//				for (unsigned int k = (i < inflateRadius ? 0 : i - inflateRadius) ; k < mapHeight && k < i + inflateRadius ; k++){
+//					for (unsigned int l = (j < inflateRadius ? 0 : j - inflateRadius) ; l < mapWidth && l < j+inflateRadius; l++){
+//						if (inflated[k][l]){
+//							cellValue = true;
+//						}
+//					}
+//				}
+				row.push_back(cellValue);
+			}
+			inflated.push_back(row);
+		}
 }
 //void convertToGrig();
 //void inflateGrig();
