b8eb51fc66173e0bcacf0fdb0840a61553789102
Dudu <oliel@yahoo.com>
2016-01-06 14:34:48 -0800

    fix course

--- DuduRobot/main.cpp
+++ DuduRobot/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 #include <libplayerc++/playerc++.h>
 #include "Map.h"
 #include "STC.h"
+#include "Config.h"
 
 using namespace std;
 using namespace PlayerCc;
@@ -25,14 +27,14 @@ int main(){
 //	}while (pp.GetYPos() > initialYPos -1);
 //	pp.SetSpeed(0,0);
 //	cout << pp.GetYaw();
-
-	float mapResolution = 0.025;
-	float robotSize = 0.3;
+	Config conf("config.file");
+	float mapResolution = strtof(conf[Config::MAP_RESOLUTION].c_str(), 0); // 0.025;
+	float robotSize = conf.getFloat(Config::ROBOT_SIZE); //strtof(conf[Config::ROBOT_SIZE].c_str(), 0); //0.3;
 
 	Map map("roboticLabMap.png",mapResolution, robotSize);
-	STC stc(map, Position(8, 9.7));
+	STC stc(map, Position(7, 9));
 
-	cout<<"Writeing output file"<<endl;
-	map.drowMapWithCourse("out.png", Position(8, 9.7));
+	cout<<"Writing output file"<<endl;
+	map.drowMapWithCourse("out.png", Position(8.4, 9.7));
 	cout<<"bye from Dudu robot";
 }
